bc987cdb2aa4b11db9de64b19a66562a471fd2fb
pavankumarmareboina <pavankumarmareboina@gmail.com>
2025-04-19 08:49:43 +0530

    Update script.js

--- js Dom/js-Dom/script.js
+++ js Dom/js-Dom/script.js
@@ -1,1 +1,50 @@
-console.log('hello js')
+// console.log('hello js')
+
+// const obj1 = {first: 20, second: 30, first: 50};
+// console.log(obj1);
+
+// let a = [1, 2, 3, 4, 5, 6];
+// let target = 5;
+
+// if (a.includes(target)) {
+//     console.log("YES");
+// } else {
+//     console.log("NO");
+// }
+
+// for (var i = 3; i <= 20; i++) {
+
+//      if (i%2 !=0){
+//         console.log(i)
+//      }
+//   }
+
+// const marks = prompt("enter your marks");
+// let student = marks
+// switch (true) {
+//   case marks > 90 && marks <= 100:
+//     score = "you will finist marks in class";
+//     break;
+//   case marks > 80 && marks <= 90:
+//     score = "you second rank in class marks";
+//     break;
+//   case marks > 70 && marks <=80:
+//     score = "you pass very good";
+//     break;
+//   case marks > 60 && marks <= 70:
+//     score = "you will pass good ";
+//     break;
+//   case marks > 50 && marks <= 70:
+//     score = "nice pass good ";
+//     break;
+//   case marks > 35 && marks <= 50:
+//     score = "you will pass improve ";
+//     break;
+//     case marks>10 && marks <=34:
+//         score = 'you will fail !!!!'
+//         break ;
+//         default:
+//             score = 'enter proper up 100 are there'
+// }
+// document.writeln(score)
+
